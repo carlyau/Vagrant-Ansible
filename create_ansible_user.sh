3e10110a5991aabf4df608f25e6d72eddb9a1482
carlyau <Carl.Yau@gmail.com>
2020-06-29 14:15:02 +0100

    Create create_ansible_user.sh

---
+++ create_ansible_user.sh
@@ -0,0 +1,92 @@
+private_key="-----BEGIN RSA PRIVATE KEY-----
+MIIJKAIBAAKCAgEA1bO1ki9wWlKICdTD2r8LUAA28iZUzPjnRmZLwAuSuOZAM5SF
+QlBDTriCWm9K7zyaGjdjCNLdUGGPVB73HmoIaDnWRqMe0uj72Mzu73/Jg/byS4OY
+uhbB2zHq/Vo6Nb2EmkfvcIFP2YgKobgu3lDtkoztVbbrT1cKq6h5VBo2BGbes2by
+ZLrPq1GO2qPwQAIDxEbEiUrmmBW7yBubgyMW1bHYXgx6GlQ+WPlo9SwzIA5UBQ+E
+CfPzXCQcPrrXmsEB9jMwmXqLUHM2svWj7d8vD5p1LhR+mTc9Cpmi9uC1LhuH4phN
+Nt33/L4D6rnlBZ/DdvoeP5QrJu3Uzqfd4mWMmtpXSHIhRcXqViShAuqI5ebIXjX0
+pDV/KBcFOJUunEXl/wBYYTPZn+CuKeyC/ylhF+JXee/BmRn6Io6JiSc0MzKBV+YX
+SIIVpMImtLEkrc81QEsFEL5s5ArHeObDyWNlcv0E4c8RD7OnDmKBk9Tnu6ZeOQyA
+S9Kkjf1iVB9glTik/YVZ1e+ZL9eQWovNKo8RnqgGy2IXdIlq1Bjan9yx2pLjpC3I
+k5HTiUo+/6UB90PEaw/7TR83SdTRCzgqbjBYZv3msFFPT/gJvtbcHih23tD4PVxp
+2O1BT8d3Ey1waxwf3P5YOcSbmV93YBkBIHIP7QF6LTDGVcY75QGu1pXfYWECAwEA
+AQKCAgB3+i8EZbHUY+WQb22OEBgP0C8JAFA3y748ng4FQfob6Nk+nDG8q0c+mQRG
+NZmCeymhclsdhxVNnZhUiBYBc1JJAb+1ZLtXegp7C4NR3xKKDki18Kn8P+6SW5Is
+r01jAoXCAIUDSvZDkMc3SCq4EgNvdcOBjfLKVnndKfcEKrsM+ZwqaK/JscAJ1P44
+7jViGj93IKwS0IRp+jm0/RrwIvEdBH4PPz4GLina1hwBGdGSO7kcJjvhQp7sqjFZ
+XpjRf27UhKTSZiR1BFo/Wv2v/M91GSiUuFjjOEoC+4KszDesLTS7GPqwxSoHMb8N
+N1THNY0N7RqyY0Ne1cPWShSemJi5CIwAnGEiYl4Oc1koYdpNzYxHBnzJ7p44aNT9
+/oFDu8k17qX+8uCsw7e3DX2vXojHMiBkB0KAcYocHX4REdY6Owstr/CuRtxuHRxI
+LqHmRVOgh2jm1wK+Ks0V4I7zpnRfWewjyDncFlm4ZELRCgRap2mJpyTMc9qMwy+b
+Ozwt1bMm/LAuCkeMGDk9FS/41gLzIsamzc96THspIcF+vxfeOhI5i8xlpgU1Z7WI
+BiZF855orn9f+fe/WPiiimUWCGdLV4mGUP2DBql31/KowamZ2xA45Mm2QMgD8dNE
+QsT1jKTYoW3LdCp3H9/lv9OZpFsfYliddpj4ZhucXJsz4gMR4QKCAQEA+dOxZTRa
+mBM56QCv44ooc6dLtHhzvfyRGs8wHWs9p5eo5bfhurfpT1anq+J/3+dsTTEiKZaw
+cG7ird3ly5wyakqHedyfnpLuGoYhU6v6LoTO7iIicUr54eGzIgXceFdaAtfjzCi9
+7ElYSYpRtDSUpUQn7EExY52g1qSjB8tBlMcfD+UwyBXc39eS2Y0zOf627zXxzcbq
+dXo/bfKA6Q9pR4Dh5FkJZhZCB/xUKVZ7GWAWcqOjN19FTFK/IczKlwxtaqIFqm95
+3FJGqPSRCIFqtkkaiDVLwLUdMemVVl4acu++qqCjs1A5kkCiuomq79qqA/BRcjuT
+MlPQsdJEsAafzQKCAQEA2vuBD8el7Q+52xWlbUEB1KIa4KOqBW5rWnaxGNobBj8v
+PVF9Vop0yY5tW2/GEpsuITlkUHC86YlflFf9QFo7hhCY/YQvzQS+zGcVHnnvELxs
+xwlSTB6byrlSgq4dwEsMSNCRm8iasYxWHB3AwFOa66Sgz4xn1w0LeYG2v/Lixhr6
+8c8FIAP38v7HZq6wq6R6q2cm8f2TVPSzzb8Npp94K0eusaXa5AON/3Xdu8aQQpMA
+GcJD/pVXHZUGOa941zJfXChWB31UapXRltmsPLuxxbPox/gxicrTSxHMOY4+HVKJ
+PIgA7Gte1s7rkdxD5p2ZumzKZbtDDOLfKvmitocr5QKCAQBnh7HxJdQRR45tH8rE
+OeHvo7m8zVnxtrQPbyMaXfHL/Q6SPDTAYyy9o0SS96dUi5wXFTB6Gad+wIUR1m4/
+hbhi0gX4m1ROq8cemM1A1nk0Q/dZOi4Z0x9g216Xz+QAvvpfSUZuuceTHwYGW9rm
+GhAruOXug0nLZWxc5xdkyHeiBy65MJqZoG3LGZQSsfAzrXn0Ad/2DePl0UZt8tw3
+ORrA9gkmzzsU4k507r2V7+/NoUNMOMIP1Vxrd7Mbd7nRBOZnFUPR0KaohjQgGlHs
+O22UoxDqt2NwNd4mbA1mIFNHpFDYdv+/9JQXJtAJC7oZ8f0Y6sNl2ohyh8dpmaC7
+bdNVAoIBAQCeUfOPixKFgDcpFE9T3AoMx16TRwCniJuI/gMwKpAbRhdLR91McjHZ
+f2XFE9yqmxMQ5kOGfNu4JJ+7ypnIqvQaC2nw8F725RVlBqoYLKQQ/4nNpQmhCwgr
+2yyyWlwcKKYrBtAfRzvAXevry/8NjeSVtaKmK6bvrfi70nJj+KDIBb2kkb3s1bGw
+N0/HB0mn7vTZc2yOzHx2kaxldlnSO71P3TWKLpVBAOjq5zZ2iirGVy1dcjxilWo0
+cwfbMOg2/a5AHNGF/LDJlqO/YoTKLBFtFJOZ7/Ew5Dk7LxTuD88MuB2Ma+/u/fnF
+aDicbhmRZCQ+ymTShD8H1AbxNSbUvImZAoIBAFTGj0gQWbLcY3YVVEP8qY9ZnXNs
+iIuqvc+68VR86OJ73GTzXPB2a/pDL+tgMa0FW5IErhilzvcqU0LICftqjqt2mXRf
++z86kIVEwIdiB0rPrmnrLTeInBdlR737ka01lyKXi77KiNwyWnD1kLzZ7SBppIhQ
+biS30sX7hRHdkYILNykn91p2SecWY+w9hnX/vpM8bHtDvrvWREp+bvQHP7VYaazx
+j/eaDEE7QaQ337/dIfJunsuLY28AHRi7AsJvY+OusW1+nGYiNTQ5CAa/wTS0hUJ8
+jsr3WJFusDCK/99//qv0twiolls3tr4BkfpfYwTwe5TvvE1tt0INN25hbA0=
+-----END RSA PRIVATE KEY-----"
+ 
+public_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDVs7WSL3BaUogJ1MPavwtQADbyJlTM+OdGZkvAC5K45kAzlIVCUENOuIJab0rvPJoaN2MI0t1QYY9UHvceaghoOdZGox7S6PvYzO7vf8mD9vJLg5i6FsHbMer9Wjo1vYSaR+9wgU/ZiAqhuC7eUO2SjO1VtutPVwqrqHlUGjYEZt6zZvJkus+rUY7ao/BAAgPERsSJSuaYFbvIG5uDIxbVsdheDHoaVD5Y+Wj1LDMgDlQFD4QJ8/NcJBw+uteawQH2MzCZeotQczay9aPt3y8PmnUuFH6ZNz0KmaL24LUuG4fimE023ff8vgPqueUFn8N2+h4/lCsm7dTOp93iZYya2ldIciFFxepWJKEC6ojl5sheNfSkNX8oFwU4lS6cReX/AFhhM9mf4K4p7IL/KWEX4ld578GZGfoijomJJzQzMoFX5hdIghWkwia0sSStzzVASwUQvmzkCsd45sPJY2Vy/QThzxEPs6cOYoGT1Oe7pl45DIBL0qSN/WJUH2CVOKT9hVnV75kv15Bai80qjxGeqAbLYhd0iWrUGNqf3LHakuOkLciTkdOJSj7/pQH3Q8RrD/tNHzdJ1NELOCpuMFhm/eawUU9P+Am+1tweKHbe0Pg9XGnY7UFPx3cTLXBrHB/c/lg5xJuZX3dgGQEgcg/tAXotMMZVxjvlAa7Wld9hYQ== "
+
+AWXRMTUSER="ansible"
+
+
+if [ $(id -u ${AWXRMTUSER} &>/dev/null;echo $?) -eq 0 ]
+then
+  #ansible is installed already
+  exit 0
+fi
+
+echo "Updating hosts file"
+cat >> /etc/hosts<<EOF
+192.168.56.200	control.example.com	control
+192.168.56.201	node1.example.com	node1
+192.168.56.202	node2.example.com	node2
+192.168.56.203	node3.example.com	node3
+192.168.56.204	node4.example.com	node4
+EOF
+
+echo "Creating ${AWXRMTUSER} user"
+useradd ${AWXRMTUSER}
+echo "${AWXRMTUSER} ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/${AWXRMTUSER}
+echo "Creating ${AWXRMTUSER} Public SSH Key"
+mkdir /home/${AWXRMTUSER}/.ssh
+chmod 700 /home/${AWXRMTUSER}/.ssh
+if [ "$(hostname)" = "control" -o "$(hostname)" = "tower" ]
+then
+  #set relevant hosts entry
+  sed -i 's/\(192.168.56.200 \).*/\1 '$(hostname)'.example.com\t'$(hostname)'/' /etc/hosts
+  echo "${private_key}" > /home/${AWXRMTUSER}/.ssh/id_rsa
+  echo "${public_key}" > /home/${AWXRMTUSER}/.ssh/id_rsa.pub
+  chmod 600 /home/${AWXRMTUSER}/.ssh/id_rsa
+fi
+echo "${public_key}" > /home/${AWXRMTUSER}/.ssh/authorized_keys
+echo 'Host *' >> /home/${AWXRMTUSER}/.ssh/config
+echo 'StrictHostKeyChecking no' >> /home/${AWXRMTUSER}/.ssh/config
+echo 'UserKnownHostsFile /dev/null' >> /home/${AWXRMTUSER}/.ssh/config
+chown -R "${AWXRMTUSER}:${AWXRMTUSER}" /home/${AWXRMTUSER}
+chmod 600 /home/${AWXRMTUSER}/.ssh/config
